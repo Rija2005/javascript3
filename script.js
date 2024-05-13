222d1a2d90747b28abce0e161510f65fa0fe36cd
Rija2005 <164767074+Rija2005@users.noreply.github.com>
2024-05-13 16:07:02 +0500

    my practice

---
+++ script.js
@@ -0,0 +1,84 @@
+//a++; //post increament
+//++a;//pre increament
+
+
+// let a =10;
+// console.log(a--);
+// console.log(a--);
+
+//prompt types;but + more use.
+
+// let number1 =+(prompt("enter your no."));
+// let number2 = +(prompt("enter your second no."));
+// console.log(number1*number2);
+
+// let number1 =Number(prompt("enter your no."));
+// let number2 = Number(prompt("enter your second no."));
+// console.log(number1*number2);
+
+
+
+// let number1 =parsent(prompt("enter your no."));
+// let number2 = parseInt(prompt("enter your second no."));
+// console.log(number1*number2);  
+
+// let number1 =Number(prompt("enter your no." ,30));
+// let number2 = Number(prompt("enter your second no."));
+// console.log(number1+number2);
+
+
+// let y= 12;
+// let z = 18;
+// console.log(y!==z);
+
+
+//if and else statement
+// let mode = "light";
+// let color = "black";
+// if(mode=="dark"){
+
+//     console.log(color);
+// }
+// else{
+//     console.log("white");
+// }
+
+
+
+// let age = +prompt("enter your age");
+// if(age > 18){
+//     console.log("eligebile for voting");
+// }
+// else{
+//     console.log(" not eligebale for voting");
+// }
+
+// let number = +prompt("Enter your number");
+// if(number %2== 0){
+//     console.log(number,"is a even number");
+
+// }
+// else{
+//     console.log(number,"is a odd number");
+// }
+
+
+// let age = +prompt("enter your age")
+// if(age <18 ){
+//     console.log("junior");
+// }//condition give
+// else if(age>60){
+// console.log("senior");
+// }//statement we give
+// else{
+//     console.log("middle");
+// }//othervise statement
+
+
+let number = +prompt("Enter your no.");
+if( number %5 == 0){
+    console.log(number, "multiple of 5");
+}
+ else{
+       console.log(number,"is a not multiple of 5");
+     }
